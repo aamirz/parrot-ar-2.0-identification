b4e4caf8d4df3a59eeb4b9cd2f9e24d9cfa2e60f
Aamir Zainulabadeen <aamirz@princeton.edu>
2017-04-11 09:37:28 -0400

    configuration script for installation

---
+++ configInstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+make identifier='\"aamir\"'
+mv ./server aamirServer
+make identifier='\"jake\"'
+mv ./server jakeServer
+make identifier='\"chetty\"'
+mv ./server chettyServer
+make identifier='\"waqa\"'
+mv ./server waqaServer
+make identifier='\"zach\"'
+mv ./server zachServer
+make identifier='\"nico\"'
+mv ./server nicoServer
