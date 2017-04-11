cde9486c054263e731e478d9402911d4b43f9a6d
vagrant <vagrant@precise32.(none)>
2017-04-11 12:51:38 +0000

    should serve multiple people

--- server.c
+++ server.c
@@ -29,23 +29,37 @@ int main(int argc, char *argv[])
      if (sockfd < 0)
         error("ERROR opening socket");
      bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
+     portno = 5560;
      serv_addr.sin_family = AF_INET;
      serv_addr.sin_addr.s_addr = INADDR_ANY;
      serv_addr.sin_port = htons(portno);
      if (bind(sockfd, (struct sockaddr *) &serv_addr,
               sizeof(serv_addr)) < 0)
               error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-     if (newsockfd < 0)
-          error("ERROR on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
-     return 0;
+
+     while(1) {
+	/* listen on the socket */ 
+	listen(sockfd,5);
+	clilen = sizeof(cli_addr);
+     	newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+
+	pid_t pid = fork();
+	
+	/* child process handles the task */
+	if (pid == 0) {
+	     	if (newsockfd < 0)
+        		  error("ERROR on accept");
+     		bzero(buffer,256);
+     		n = read(newsockfd,buffer,255);
+     		if (n < 0) error("ERROR reading from socket");
+     		printf("Here is the message: %s\n",buffer);
+     		n = write(newsockfd,"I got your message",18);
+     		if (n < 0) error("ERROR writing to socket");
+		break;
+		}
+		else {
+			continue;
+		}
+	}
+	return 0;
 }
