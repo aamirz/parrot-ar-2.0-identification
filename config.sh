9b9193d0eaba3f2744b4d0d46ca97709ef78dd48
vagrant <vagrant@precise32.(none)>
2017-04-25 14:40:00 +0000

    modified config to reflect necessity

--- config.sh
+++ config.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+mkdir /id-servers
 NUM=$1
 chmod +x forkedServer_drone
 mv drone_id_$NUM.txt drone_id.txt
-mv forkedServer_drone /id-servers
+mv forkedServer_drone forkedServer
+mv forkedServer /id-servers
 mv drone_id.txt /id-servers
